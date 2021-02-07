771f7c81c031472d9cc54e20a4bccb8251743e44
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-07 23:18:43 +0000

    Don't capitalize 'catalogue'

--- src/views/database/rhoads/GroverJacksonRRCustom.vue
+++ src/views/database/rhoads/GroverJacksonRRCustom.vue
@@ -53,12 +53,12 @@
       <h2 class="title is-4">Catalogue Links</h2>
       <p>
         <a href="https://www.vintagejapanguitars.com.br/en/jackson-charvel-1990-japan-catalogue/">
-          Charvel &#38; Grover Jackson Catalogue (June 1990, Japan)
+          Charvel &#38; Grover Jackson catalogue (June 1990, Japan)
         </a>
       </p>
       <p>
         <a href="https://assets.ctfassets.net/4jcppgetbqrc/54C6GyN9W8wYuGOekKMgoe/c181e59f1da5f4158123dc1d77044774/JacksonCharvel-1993GJ-Japan-catalog.pdf">
-          Grover Jackson &#38; Charvel Catalogue (1993, Japan)
+          Grover Jackson &#38; Charvel catalogue (1993, Japan)
         </a>
       </p>
     </div>
