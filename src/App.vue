0ddcfbfa151f80751280933c28df2d4e19f0184c
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-07 19:12:35 +0000

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Header activeMenuSection="database"/>
     <Body activeMenuSection="database"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
