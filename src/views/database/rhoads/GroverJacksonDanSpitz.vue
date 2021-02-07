1c3f1b591a58aa0a495d7718e6ec0a75e807fe99
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-07 22:46:55 +0000

    Fill in Grover Jackson Dan Spitz page

--- src/views/database/rhoads/GroverJacksonDanSpitz.vue
+++ src/views/database/rhoads/GroverJacksonDanSpitz.vue
@@ -3,15 +3,72 @@
   
     <Title title="Grover Jackson Dan Spitz Professional" subtitle="1991-1994"/>
 
+    <!-- tags -->
+    <div class="block">
+      <span class="tag is-primary">Early 90s</span>
+      <span class="tag is-danger">Japanese Market Exclusive</span>
+      <span class="tag is-link">MOP Inlays</span>
+    </div>
+
+    <!-- overview -->
+    <div class="block">
+      <h2 class="title is-4">Overview</h2>
+      <p>
+        Signature model of Anthrax's Dan Spitz.
+      </p>
+    </div>
+
+    <!-- specs -->
+    <div class="block">
+      <h2 class="title is-4">Specs</h2>
+      <p><strong>Construction:</strong> Neck-through body</p>
+      <p><strong>Scale:</strong> 25.5" (648 mm)</p>
+      <p><strong>Number of frets:</strong> 22</p>
+      <p><strong>Body:</strong> Yellow Poplar</p>
+      <p><strong>Neck:</strong> Quartersawn Maple</p>
+      <p><strong>Fingerboard:</strong> Ebony</p>
+      <p><strong>Inlays:</strong> MOP Reverse Sharkfins</p>
+      <p><strong>Binding:</strong> Ivory</p>
+      <p><strong>Fret Wire:</strong> Rockwell C6/Brinell 171</p>
+      <p><strong>Fret Size:</strong> .118 x .114 x .051 x .037</p>
+      <p><strong>Pickups:</strong> EMG-81 (neck), EMG-85 (bridge)</p>
+      <p><strong>Controls:</strong> 1x Volume, 3-Way Toggle Switch</p>
+      <p><strong>Tuners:</strong> Grover Jackson SG38-06</p>
+      <p><strong>Bridge:</strong> Jackson-branded Schaller double locking tremolo</p>
+      <p><strong>Bridge Mount:</strong> Non-recessed with full stretch cavity</p>
+      <p><strong>Finishes:</strong> Black</p>
+      <p><strong>Hardware:</strong> Gold</p>
+      <p><strong>Price:</strong> &#165;230,000 (with hard case)</p>
+    </div>
+
+    <Gallery/>
+
+    <!-- catalogue links -->
+    <div class="block">
+      <h2 class="title is-4">Catalogue Links</h2>
+      <p>
+        <a href="https://www.vintagejapanguitars.com.br/en/jackson-charvel-1990-japan-catalogue/">
+          Charvel &#38; Grover Jackson Catalogue (June 1990, Japan)
+        </a>
+      </p>
+      <p>
+        <a href="https://assets.ctfassets.net/4jcppgetbqrc/54C6GyN9W8wYuGOekKMgoe/c181e59f1da5f4158123dc1d77044774/JacksonCharvel-1993GJ-Japan-catalog.pdf">
+          Grover Jackson &#38; Charvel Catalogue (1993, Japan)
+        </a>
+      </p>
+    </div>
+
   </article>  
 </template>
 
 <script>
+import Gallery from '../../../components/Gallery.vue'
 import Title from '../../../components/Title.vue'
 
 export default {
   name: 'GroverJacksonDanSpitz',
   components: {
+    Gallery,
     Title
   }
 }
