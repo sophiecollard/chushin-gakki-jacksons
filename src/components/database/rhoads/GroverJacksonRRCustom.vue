df92f4356db285b6fb8d1b87e7d88a2095d9f664
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-07 18:41:05 +0000

    Move page contents to their own components

---
+++ src/components/database/rhoads/GroverJacksonRRCustom.vue
@@ -0,0 +1,120 @@
+<template>
+  <article>
+
+    <!-- title -->
+    <h1 class="title">Grover Jackson Randy Rhoads Custom</h1>
+    <p class="subtitle">1990-1994</p>
+
+    <!-- tags -->
+    <div class="block">
+      <span class="tag is-primary">Early 90s</span>
+      <span class="tag is-danger">Japanese Market Exclusive</span>
+      <span class="tag is-link">MOP Inlays</span>
+    </div>
+
+    <!-- overview -->
+    <div class="block">
+      <h2 class="title is-4">Overview</h2>
+      <p>
+        A high-end Japanese-made model released for the domestic market only. 
+        While the first ones came with 22 frets, some time around 1991 Jackson updated this model to 24 frets, making it (to my knowledge) the first 24-frets Rhoads catalogue model.
+      </p>
+    </div>
+
+    <!-- specs -->
+    <div class="block">
+      <h2 class="title is-4">Specs</h2>
+      <p><strong>Construction:</strong> Neck-through body</p>
+      <p><strong>Scale:</strong> 25.5" (648 mm)</p>
+      <p><strong>Number of frets:</strong> <i>As per 1991 catalogue:</i> 22</p>
+      <p><strong style="color: white;">Number of frets:</strong> <i>As per 1993 catalogue:</i> 24</p>
+      <p><strong>Body:</strong> Yellow Poplar</p>
+      <p><strong>Neck:</strong> Quartersawn Maple</p>
+      <p><strong>Fingerboard:</strong> Ebony</p>
+      <p><strong>Inlays:</strong> MOP Sharkfins</p>
+      <p><strong>Fret Wire:</strong> Rockwell C6/Brinell 171</p>
+      <p><strong>Fret Size:</strong> .118 x .114 x .051 x .037</p>
+      <p><strong>Pickups:</strong> <i>As per 1991 catalogue:</i> J-50N (neck), J-50B (bridge)</p>
+      <p><strong style="color: white;">Pickups:</strong> <i>As per 1993 catalogue:</i> J-50BC (neck), J-90C (bridge)</p>
+      <p><strong>Controls:</strong> 2x Volume, 1x Tone, 3-Way Toggle Switch</p>
+      <p><strong>Tuners:</strong> Grover Jackson SG38-06</p>
+      <p><strong>Bridge:</strong> Jackson-branded Schaller double locking tremolo</p>
+      <p><strong>Bridge Mount:</strong> Non-recessed with full stretch cavity</p>
+      <p><strong>Finishes:</strong> <i>As per 1991 catalogue:</i> Pearl Gold, Metallic Black, Pearl Blue White</p>
+      <p><strong style="color: white;">Finishes:</strong> <i>As per 1993 catalogue:</i> Metallic Black, Pearl Blue White, Aluminium, Dark Blue</p>
+      <p><strong>Price:</strong> <i>As per 1991 catalogue:</i> &#165;195,000 (with hard case)</p>
+      <p><strong style="color: white;">Price:</strong> <i>As per 1993 catalogue:</i> &#165;200,000 (with hard case)</p>
+    </div>
+
+    <!-- gallery -->
+    <div class="block">
+      <h2 class="title is-4">Gallery</h2>
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+        <div class="column">
+          <figure class="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png">
+          </figure>
+        </div>
+      </div>
+    </div>
+
+    <!-- catalogue links -->
+    <div class="block">
+      <h2 class="title is-4">Catalogue Links</h2>
+      <p>
+        <a href="https://brochures.yokochou.com/guitar-and-amp/groverjackson/1991/en_index.html">
+          Charvel &#38; Grover Jackson Catalogue (April 1991, Japan)
+        </a>
+      </p>
+      <p>
+        <a href="https://assets.ctfassets.net/4jcppgetbqrc/54C6GyN9W8wYuGOekKMgoe/c181e59f1da5f4158123dc1d77044774/JacksonCharvel-1993GJ-Japan-catalog.pdf">
+          Grover Jackson &#38; Charvel Catalogue (1993, Japan)
+        </a>
+      </p>
+    </div>
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'GroverJacksonRRCustom'
+}
+</script>
---
+++ src/components/database/rhoads/GroverJacksonRRProfessional.vue
@@ -0,0 +1,15 @@
+<template>
+  <article>
+  
+    <!-- title -->
+    <h1 class="title">Grover Jackson Randy Rhoads Professional</h1>
+    <p class="subtitle">1990-1994</p>
+
+  </article>  
+</template>
+
+<script>
+export default {
+  name: 'GroverJacksonRRProfessional'
+}
+</script>
