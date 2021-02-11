0d66a7230c05c021b8c69de7219e819ac620fcfd
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-11 21:47:16 +0000

    Add jackson rr professional details

--- src/views/database/rhoads/jackson-rr-professional/Main.vue
+++ src/views/database/rhoads/jackson-rr-professional/Main.vue
@@ -4,24 +4,30 @@
     <div class="column is-two-thirds">
       <Title title="Jackson Randy Rhoads Pro/Professional" subtitle="1990-1995"/>
       <Tags/>
+      <Specs/>
+      <CatalogueLinks/>
     </div>
 
     <div class="column is-one-third">
-      <Extras catalogueShot="http://www.redox.com.ua/uploads/posts/2010-05/1274855416_2913024899v1_hi-dfb2c7d3b7d9254c8afb61a16f6ee941.jpg"/>
+      <Extras catalogueShot="/database/jackson-rr-professional.png"/>
     </div>
 
   </div>  
 </template>
 
 <script>
+import CatalogueLinks from './CatalogueLinks.vue'
 import Extras from '../../../../components/Extras.vue'
+import Specs from './Specs.vue'
 import Tags from './Tags.vue'
 import Title from '../../../../components/Title.vue'
 
 export default {
   name: 'JacksonRRProfessional',
   components: {
+    CatalogueLinks,
     Extras,
+    Specs,
     Tags,
     Title
   }
---
+++ src/views/database/rhoads/jackson-rr-professional/Specs.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="block">
+    <h2 class="title is-4">Specs</h2>
+    <p><strong>Construction:</strong> Neck-through</p>
+    <p><strong>Scale:</strong> 25.5" (648 mm)</p>
+    <p><strong>Number of frets:</strong> 22</p>
+    <p><strong>Body:</strong> Poplar</p>
+    <p><strong>Neck:</strong> Quartersawn Maple</p>
+    <p><strong>Fingerboard:</strong> Ebony</p>
+    <p><strong>Inlays:</strong> MOP Sharkfins</p>
+    <p><strong>Binding:</strong> Ivory</p>
+    <p><strong>Pickups:</strong> J-50N (neck), J-50B (bridge), JE-1200 Active Mid Boost Circuit</p>
+    <p><strong>Controls:</strong> Volume, Mid Boost, Tone, 3-Way Toggle Switch</p>
+    <p><strong>Bridge:</strong> Jackson-branded Schaller (JT-590) double locking tremolo</p>
+    <p><strong>Bridge Mount:</strong> Recessed</p>
+    <p><strong>Finishes:</strong> <i>As per 1992 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White)</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>As per 1993 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White), DMR (Dark Metallic Red)</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>As per 1994/5 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), BC (?)</p>
+    <p><i>In addition to the finishes listed above, the following are also common and can be seen in some of the catalogue shots:</i> White with Black Bevels, Yellow with Black Bevels, Ferrari Red with Black Bevels</p>
+    <p><strong>Hardware:</strong> Black</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Specs'
+}
+</script>
