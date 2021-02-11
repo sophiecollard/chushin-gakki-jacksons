1ecae221feff7030ef9cc259a2ea67c6943bb80c
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-11 21:11:24 +0000

    Add catalogue shots

--- src/views/database/rhoads/grover-jackson-rr-s-350/Main.vue
+++ src/views/database/rhoads/grover-jackson-rr-s-350/Main.vue
@@ -8,7 +8,7 @@
     </div>
 
     <div class="column is-one-third">
-      <Extras catalogueShot="http://www.redox.com.ua/uploads/posts/2010-05/1274855416_2913024899v1_hi-dfb2c7d3b7d9254c8afb61a16f6ee941.jpg"/>
+      <Extras catalogueShot="/database/grover-jackson-rr-s-350.png"/>
     </div>
 
   </div>  
