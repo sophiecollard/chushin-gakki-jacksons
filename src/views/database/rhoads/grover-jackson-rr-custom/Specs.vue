897ac9f0061f219ea4fd9eb3d0ac4eb8681eddd7
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-12 00:45:46 +0000

    Update RR custom specs

--- src/views/database/rhoads/grover-jackson-rr-custom/Specs.vue
+++ src/views/database/rhoads/grover-jackson-rr-custom/Specs.vue
@@ -4,7 +4,7 @@
     <p><strong>Construction:</strong> Neck-through</p>
     <p><strong>Scale:</strong> 25.5" (648 mm)</p>
     <p><strong>Number of frets:</strong> <i>As per 1990 catalogue:</i> 22</p>
-    <p><strong style="color: white;">Number of frets:</strong> <i>As per 1993 catalogue:</i> 24</p>
+    <p><strong style="color: white;">Number of frets:</strong> <i>As per 1992/93 catalogues:</i> 24</p>
     <p><strong>Body:</strong> Yellow Poplar</p>
     <p><strong>Neck:</strong> Quartersawn Maple</p>
     <p><strong>Fingerboard:</strong> Ebony</p>
@@ -13,16 +13,17 @@
     <p><strong>Fret Wire:</strong> Rockwell C6/Brinell 171</p>
     <p><strong>Fret Size:</strong> .118 x .114 x .051 x .037</p>
     <p><strong>Pickups:</strong> <i>As per 1990 catalogue:</i> J-50N (neck), J-50B (bridge)</p>
-    <p><strong style="color: white;">Pickups:</strong> <i>As per 1993 catalogue:</i> J-50BC (neck), J-90C (bridge)</p>
+    <p><strong style="color: white;">Pickups:</strong> <i>As per 1992/93 catalogues:</i> J-50BC (neck), J-90C (bridge)</p>
     <p><strong>Controls:</strong> 2x Volume, 1x Tone, 3-Way Toggle Switch</p>
     <p><strong>Tuners:</strong> Grover Jackson SG38-06</p>
     <p><strong>Bridge:</strong> Jackson-branded Schaller double locking tremolo</p>
     <p><strong>Bridge Mount:</strong> Non-recessed with full stretch cavity</p>
-    <p><strong>Finishes:</strong> <i>As per 1990 catalogue:</i> Pearl Gold, Metallic Black, Pearl Blue White</p>
+    <p><strong>Finishes:</strong> <i>As per 1990 catalogue:</i> Metallic Black, Pearl Blue White, Pearl Gold</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>As per 1992 catalogue:</i> Metallic Black, Pearl Blue White, Pearl Yellow, Dark Blue</p>
     <p><strong style="color: white;">Finishes:</strong> <i>As per 1993 catalogue:</i> Metallic Black, Pearl Blue White, Aluminium, Dark Blue</p>
     <p><strong>Hardware:</strong> Black</p>
     <p><strong>Price:</strong> <i>As per 1990 catalogue:</i> &#165;195,000 (with hard case)</p>
-    <p><strong style="color: white;">Price:</strong> <i>As per 1993 catalogue:</i> &#165;200,000 (with hard case)</p>
+    <p><strong style="color: white;">Price:</strong> <i>As per 1992/93 catalogues:</i> &#165;200,000 (with hard case)</p>
   </div>
 </template>
 
