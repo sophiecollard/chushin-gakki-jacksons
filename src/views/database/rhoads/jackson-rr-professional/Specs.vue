429c9b40aead7762dbb7c8f832b2865b604febdc
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-12 00:31:17 +0000

    Update RR pro finishes list

--- src/views/database/rhoads/jackson-rr-professional/Specs.vue
+++ src/views/database/rhoads/jackson-rr-professional/Specs.vue
@@ -13,10 +13,10 @@
     <p><strong>Controls:</strong> Volume, Mid Boost, Tone, 3-Way Toggle Switch</p>
     <p><strong>Bridge:</strong> Jackson-branded Schaller (JT-590) double locking tremolo</p>
     <p><strong>Bridge Mount:</strong> Recessed</p>
-    <p><strong>Finishes:</strong> <i>As per 1992 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White)</p>
+    <p><strong>Finishes:</strong> <i>As per 1990/91 catalogue:</i> Midnight Black, Ferrari Red, Pearl Yellow, Pearl Yellow with Black Bevels, Snow White Pearl with Black Bevels, Radiant Red with Black Bevels</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>As per 1992 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White)</p>
     <p><strong style="color: white;">Finishes:</strong> <i>As per 1993 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White), DMR (Dark Metallic Red)</p>
     <p><strong style="color: white;">Finishes:</strong> <i>As per 1994/5 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), BC (?)</p>
-    <p><i>In addition to the finishes listed above, the following are also common and can be seen in some of the catalogue shots:</i> White with Black Bevels, Yellow with Black Bevels, Ferrari Red with Black Bevels</p>
     <p><strong>Hardware:</strong> Black</p>
   </div>
 </template>
