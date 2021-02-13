e108d3a80c83fadcc6e7aaf3fe5b2fc88eab1023
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-13 17:16:07 +0000

    Update rr pro specs

--- src/views/database/rhoads/jackson-rr-pro/Specs.vue
+++ src/views/database/rhoads/jackson-rr-pro/Specs.vue
@@ -9,14 +9,16 @@
     <p><strong>Fingerboard:</strong> Ebony</p>
     <p><strong>Inlays:</strong> Mother of Pearl Sharkfins</p>
     <p><strong>Binding:</strong> Ivory</p>
-    <p><strong>Pickups:</strong> J-50N (neck), J-50B (bridge), JE-1200 Active Mid Boost Circuit</p>
+    <p><strong>Pickups:</strong> <i>1990-1991:</i> J-50N (neck), J-50B (bridge)</p>
+    <p><strong style="color: white;">Pickups:</strong> <i>1992-1995:</i> J-50N (neck), J-50M (bridge)</p>
+    <p><strong>Active Electronics:</strong> JE-1200 Active Mid Boost Circuit</p>
     <p><strong>Controls:</strong> Volume, Mid Boost, Tone, 3-Way Toggle Switch</p>
     <p><strong>Bridge:</strong> Jackson-branded Schaller (JT-590) double locking tremolo</p>
     <p><strong>Bridge Mount:</strong> Recessed</p>
-    <p><strong>Finishes:</strong> <i>As per 1990/91 catalogue:</i> Midnight Black, Ferrari Red, Pearl Yellow, Pearl Yellow with Black Bevels, Snow White Pearl with Black Bevels, Radiant Red with Black Bevels</p>
-    <p><strong style="color: white;">Finishes:</strong> <i>As per 1992 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White)</p>
-    <p><strong style="color: white;">Finishes:</strong> <i>As per 1993 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White), DMR (Dark Metallic Red)</p>
-    <p><strong style="color: white;">Finishes:</strong> <i>As per 1994/5 catalogue:</i> BLK (Black), DMB (Dark Metallic Blue), BC (?)</p>
+    <p><strong>Finishes:</strong> <i>1990-1991:</i> Midnight Black, Ferrari Red, Pearl Yellow, Pearl Yellow with Black Bevels, Snow White Pearl with Black Bevels, Radiant Red with Black Bevels</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>1992:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White)</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>1993:</i> BLK (Black), DMB (Dark Metallic Blue), SW (Snow White), DMR (Dark Metallic Red)</p>
+    <p><strong style="color: white;">Finishes:</strong> <i>1994-1995:</i> BLK (Black), DMB (Dark Metallic Blue), BC (?)</p>
     <p><strong>Hardware:</strong> Black</p>
   </div>
 </template>
