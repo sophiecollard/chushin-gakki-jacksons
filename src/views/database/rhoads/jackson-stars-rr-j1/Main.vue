017a2235bdd0d12163fd789df3b825ca342c272e
Sophie Collard <sophie.collard@hotmail.fr>
2021-02-18 18:27:44 +0000

    Add date on RR-J1

--- src/views/database/rhoads/jackson-stars-rr-j1/Main.vue
+++ src/views/database/rhoads/jackson-stars-rr-j1/Main.vue
@@ -2,7 +2,7 @@
   <div class="columns">
   
     <div class="column is-two-thirds">
-      <Title title="Jackson Stars RR-J1" subtitle="?-2007"/>
+      <Title title="Jackson Stars RR-J1" subtitle="2002-2007"/>
       <Tags/>
       <Overview/>
       <Specs/>
