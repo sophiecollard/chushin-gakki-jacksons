729b6be599a0ef86e6b19dc218d0a85d3cc91d6e
Sophie Collard <sophie.collard@hotmail.fr>
2022-01-12 20:09:17 +0000

    Add container

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,28 @@
 <template>
   <section class="hero is-dark">
     <!-- title -->
-    <div class="hero-body">
-      <p class="title">
-        Chushin Gakki Jacksons &#38; Charvels
-      </p>
-      <p class="subtitle">
-        An opinionated guide to Japanese-made Jackson and Charvel guitars from the 90s and 2000s
-      </p>
-    </div>
-    <!-- menu -->
-    <!-- <div class="hero-footer">
-      <div class="tabs">
-        <ul>
-          <li class="is-active"><a>Model Database</a></li>
-          <li><a>Catalogue Scans</a></li>
-          <li><a>Reviews</a></li>
-        </ul>
+    <div class="container">
+
+      <div class="hero-body">
+        <p class="title">
+          Chushin Gakki Jacksons &#38; Charvels
+        </p>
+        <p class="subtitle">
+          An opinionated guide to Japanese-made Jackson and Charvel guitars from the 90s and 2000s
+        </p>
       </div>
-    </div> -->
+      <!-- menu -->
+      <!-- <div class="hero-footer">
+        <div class="tabs">
+          <ul>
+            <li class="is-active"><a>Model Database</a></li>
+            <li><a>Catalogue Scans</a></li>
+            <li><a>Reviews</a></li>
+          </ul>
+        </div>
+      </div> -->
+
+    </div>
   </section>
 </template>
 
