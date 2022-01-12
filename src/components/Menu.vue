9b1f4173415ddacf718db56db21bc7f950d8d95e
Sophie Collard <sophie.collard@hotmail.fr>
2022-01-12 18:33:07 +0000

    Sort menu alphabetically

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -5,38 +5,38 @@
       <li>
         <a v-on:click=toggleSoloistSection>Soloist</a>
         <ul v-show=soloistSectionExpanded>
-          <li><router-link to="/db/sl/jackson-soloist-pro">Jackson Soloist Pro</router-link></li>
-          <li><router-link to="/db/sl/jackson-soloist-archtop-pro">Jackson Soloist Archtop Pro</router-link></li>
-          <li><router-link to="/db/sl/jackson-soloist-xl">Jackson Soloist XL</router-link></li>
-          <li><router-link to="/db/sl/jackson-slsmg">Jackson SLSMG</router-link></li>
           <li><router-link to="/db/sl/jackson-sls3">Jackson SLS3</router-link></li>
+          <li><router-link to="/db/sl/jackson-slsmg">Jackson SLSMG</router-link></li>
+          <li><router-link to="/db/sl/jackson-soloist-archtop-pro">Jackson Soloist Archtop Pro</router-link></li>
+          <li><router-link to="/db/sl/jackson-soloist-pro">Jackson Soloist Pro</router-link></li>
+          <li><router-link to="/db/sl/jackson-soloist-xl">Jackson Soloist XL</router-link></li>
         </ul>
       </li>
       <li>
         <a v-on:click=toggleRhoadsSection>Rhoads</a>
         <ul v-show=rhoadsSectionExpanded>
+          <li><router-link to="/db/rr/grover-jackson-dan-spitz">Grover Jackson Dan Spitz Professional</router-link></li>
+          <li><router-link to="/db/rr/grover-jackson-rr-custom">Grover Jackson RR Custom</router-link></li>
+          <li><router-link to="/db/rr/grover-jackson-rr-professional">Grover Jackson RR Standard/Professional</router-link></li>
+          <li><router-link to="/db/rr/grover-jackson-rr-i170">Grover Jackson RR .I-170</router-link></li>
+          <li><router-link to="/db/rr/grover-jackson-rr-i195">Grover Jackson RR .I-195</router-link></li>
+          <li><router-link to="/db/rr/grover-jackson-rr-s350">Grover Jackson RR .S-350</router-link></li>
+          <li><router-link to="/db/rr/jackson-kevin-bond">Jackson Kevin Bond Signature Rhoads</router-link></li>
+          <li><router-link to="/db/rr/jackson-matt-tuck">Jackson Matt Tuck Signature Rhoads</router-link></li>
           <li><router-link to="/db/rr/jackson-rr-elite-fsr">Jackson Rhoads Elite FSR</router-link></li>
           <li><router-link to="/db/rr/jackson-rr-pro">Jackson Rhoads Pro</router-link></li>
           <li><router-link to="/db/rr/jackson-rr24">Jackson RR24</router-link></li>
           <li><router-link to="/db/rr/jackson-rr24m">Jackson RR24M</router-link></li>
           <li><router-link to="/db/rr/jackson-rr5">Jackson RR5</router-link></li>
           <li><router-link to="/db/rr/jackson-rr5fr">Jackson RR5FR</router-link></li>
-          <li><router-link to="/db/rr/jackson-kevin-bond">Jackson Kevin Bond Signature Rhoads</router-link></li>
-          <li><router-link to="/db/rr/jackson-matt-tuck">Jackson Matt Tuck Signature Rhoads</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-dan-spitz">Grover Jackson Dan Spitz Professional</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-rr-professional">Grover Jackson RR Standard/Professional</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-rr-custom">Grover Jackson RR Custom</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-rr-i195">Grover Jackson RR .I-195</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-rr-i170">Grover Jackson RR .I-170</router-link></li>
-          <li><router-link to="/db/rr/grover-jackson-rr-s350">Grover Jackson RR .S-350</router-link></li>
           <li><router-link to="/db/rr/jackson-stars-rr-j1">Jackson Stars RR-J1</router-link></li>
           <li><router-link to="/db/rr/jackson-stars-rr-j2">Jackson Stars RR-J2</router-link></li>
           <li><router-link to="/db/rr/jackson-stars-rr-j2sp">Jackson Stars RR-J2SP</router-link></li>
           <li><router-link to="/db/rr/jackson-stars-rr-tn01">Jackson Stars RR-TN01</router-link></li>
           <li><router-link to="/db/rr/jackson-stars-rr-tn02">Jackson Stars RR-TN02</router-link></li>
-          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-swirl">Jackson Stars RR-TN02STB LTD "Swirl" (Limited Edition)</router-link></li>
-          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-ash">Jackson Stars RR-TN02STB ASH (Limited Edition)</router-link></li>
-          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-wal">Jackson Stars RR-TN02STB WAL (Limited Edition)</router-link></li>
+          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-swirl">Jackson Stars RR-TN02STB LTD</router-link></li>
+          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-ash">Jackson Stars RR-TN02STB ASH</router-link></li>
+          <li><router-link to="/db/rr/jackson-stars-rr-tn02stb-wal">Jackson Stars RR-TN02STB WAL</router-link></li>
         </ul>
       </li>
       <!-- <li>
