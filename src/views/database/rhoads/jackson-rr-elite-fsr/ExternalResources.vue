04e2a3a3b674aab370e5df3a6a0abf474a87146d
Sophie Collard <sophie.collard@hotmail.fr>
2022-01-12 18:21:49 +0000

    Refactor ExternalResources component

--- src/views/database/rhoads/jackson-rr-elite-fsr/ExternalResources.vue
+++ src/views/database/rhoads/jackson-rr-elite-fsr/ExternalResources.vue
@@ -1,37 +1,17 @@
 <template>
   <div class="block">
     <h2 class="title is-4">External Resources</h2>
-    <h2 class="title is-6">Past Listings</h2>
-    <p>
-      <a href="https://reverb.com/item/12300106-jackson-rhoads-elite-fsr-limited-electric-guitar-black-gold-bevels-w-case">
-        Floyd-equipped version in Black with Yellow Pinstripes (Reverb)
-      </a>
-    </p>
-    <p>
-      <a href="https://www.chicagomusicexchange.com/products/jackson-rhoads-elite-black-2012-1076633">
-        Hard-tail version in Black with Yellow Pinstripes (Chicago Music Exchange)
-      </a>
-    </p>
-    <p>
-      <a href="https://reverb.com/item/24739233-jackson-rhoads-elite-fsr-2011-silver-sparkle-with-black-bevels">
-        Floyd-equipped version in Silver Sparkle with Black Bevels (Reverb)
-      </a>
-    </p>
-    <p>
-      <a href="https://reverb.com/item/26644517-2011-jackson-rhoads-elite-fsr-limited-in-silver-sparkle">
-        Hard-tail version in Silver Sparkle with Black Bevels (Reverb)
-      </a>
-    </p>
-    <p>
-      <a href="https://reverb.com/item/19237778-jackson-rhoads-elite-fsr-limited-2011-black-flame">
-        Floyd-equipped version in Transparent Black (Reverb)
-      </a>
-    </p>
+    <PastListings/>
   </div>
 </template>
 
 <script>
+import PastListings from './PastListings.vue'
+
 export default {
-  name: 'ExternalResources'
+  name: 'ExternalResources',
+  components: {
+    PastListings
+  }
 }
 </script>
--- src/views/database/rhoads/jackson-rr-elite-fsr/Intro.vue
+++ src/views/database/rhoads/jackson-rr-elite-fsr/Intro.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block">
     <p>
-      An elusive limited run from 2011.
+      A limited run from 2011.
       I have not been able to find any reference to this model in catalogues, so I pieced together the specs from Reverb listings.
     </p>
   </div>
